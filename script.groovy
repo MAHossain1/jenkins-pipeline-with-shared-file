445f9160c7ad48dbc5808de22266eebe64b348b9
MAHossain1 <mahpro110@gmail.com>
2025-03-07 12:00:41 +0600

    allah vorosa build

--- script.groovy
+++ script.groovy
@@ -12,7 +12,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image"
-    withCredentials([usernamePassword(credentialsId: 'docker_hub_repo', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+    withCredentials([usernamePassword(credentialsId: 'my-dockerhub-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         sh 'docker build -t arman04/java-maven-app:jma-3.0 .'
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
         sh 'docker push arman04/java-maven-app:jma-3.0'
